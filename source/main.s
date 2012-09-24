aa7d6d81c8f069ad5b6550893a561d91ad25835a
robin <patrice@tisserand.org>
2012-09-24 23:03:13 +0800

    source/main.s: added LED blinking

--- source/main.s
+++ source/main.s
@@ -7,12 +7,29 @@ _start:
 	lsl r1,#18		/* on GPIO n + 6 */
 	str r1,[r0,#4]		/* with n = 10 (second bank) */
 
-	/* now turn on LED */
-	mov r1,#1		/* Turn on */
+	/* play with LED */
+	mov r1,#1
 	lsl r1,#16
-	str r1,[r0,#40]
+
 loop$:
+	/* delay before turning on */
+	mov r2,#0x3F0000
+wait1$:
+	sub r2,#1
+	cmp r2,#0
+	bne wait1$
+
+	/* turn on */
+	str r1,[r0,#40]
+
+	/* delay before turning off */
+	mov r2,#0x3F0000
+wait2$:
+	sub r2,#1
+	cmp r2,#0
+	bne wait2$
+	
+	/* turn off */
+	str r1,[r0,#28]
+
 	b loop$
-	
-	
-	
